033834d0ead8fabbfdf9951be512ce77d86e125d
StevenZack <stevenzack@qq.com>
2018-07-20 22:57:18 +0800

    add Handle()

--- mux.go
+++ mux.go
@@ -9,19 +9,19 @@ import (
 
 type Server struct {
 	MySelf *http.Server
-	r      *Router
+	r      *router
 }
 
-type Router struct {
+type router struct {
 	m map[string]func(http.ResponseWriter, *http.Request)
 }
 
-func NewRouter() *Router {
-	return &Router{m: make(map[string]func(http.ResponseWriter, *http.Request))}
+func newRouter() *router {
+	return &router{m: make(map[string]func(http.ResponseWriter, *http.Request))}
 }
 func NewServer(addr string) *Server {
 	s := &Server{}
-	r := NewRouter()
+	r := newRouter()
 	s.MySelf = &http.Server{Addr: addr, Handler: r}
 	s.r = r
 	return s
@@ -43,17 +43,17 @@ func (mainServer *Server) Stop() error {
 func (mainServer *Server) HandleFunc(url string, f func(http.ResponseWriter, *http.Request)) {
 	mainServer.r.m[url] = f
 }
-func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if h, ok := router.m[r.URL.String()]; ok {
+func (s *Server) Handle(pattern string, h http.Handler) {
+	s.r.m[pattern] = h.ServeHTTP
+}
+func (rt *router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if h, ok := rt.m[r.URL.String()]; ok {
 		h(w, r)
-	} else if k, ok := hasPreffixInMap(router.m, r.URL.String()); ok {
-		router.m[k](w, r)
+	} else if k, ok := hasPreffixInMap(rt.m, r.URL.String()); ok {
+		rt.m[k](w, r)
 	} else {
 		fmt.Fprint(w, `<!DOCTYPE html><html><head><title>404</title><meta charset="utf-8"><meta name="viewport" content="width=device-width"></head><body>404 not found</body></html>`)
 	}
-}
-func (r *Router) HandleFunc(url string, f func(http.ResponseWriter, *http.Request)) {
-	r.m[url] = f
 }
 func hasPreffixInMap(m map[string]func(http.ResponseWriter, *http.Request), p string) (string, bool) {
 	for k, _ := range m {
